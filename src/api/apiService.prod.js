9c387dab3bfe1afb65e042c2249088d4ed9fe8c5
mariusespejo <espejo.marius@gmail.com>
2017-08-24 23:00:34 -0700

    minor changes

--- src/api/apiService.prod.js
+++ src/api/apiService.prod.js
@@ -1,26 +1,25 @@
 const API_BASE = '/api';
 export const EXAMPLE_ENDPOINT = API_BASE + '/example';
 
-const fetchOptions = {
+const commonFetchConfig = {
   headers: {
     Accept: 'application/json;charset=UTF-8',
     'Content-Type': 'application/json;charset=UTF-8'
   },
-  credentials: 'include',
-  method: 'GET'
+  credentials: 'include'
 };
 
-const post = body => ({
-  ...fetchOptions,
-  meathod: 'POST',
-  body: JSON.stringify(body)
-});
-
 export const getExample = () =>
-  fetch(EXAMPLE_ENDPOINT, fetchOptions).then(handleHttpResponse);
+  fetch(EXAMPLE_ENDPOINT, { ...commonFetchConfig, method: 'GET' }).then(
+    handleHttpResponse
+  );
 
 export const postExample = data =>
-  fetch(EXAMPLE_ENDPOINT, post(data)).then(handleHttpResponse);
+  fetch(EXAMPLE_ENDPOINT, {
+    ...commonFetchConfig,
+    method: 'POST',
+    body: JSON.stringify(data)
+  }).then(handleHttpResponse);
 
 const handleHttpResponse = response => {
   if (response.ok) {
